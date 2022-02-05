c00ed79111b88d4fe6a983851b108b8bfc8fdb90
David Gras <davidgrcr@gmail.com>
2022-02-05 21:35:31 +0100

    moving fetch

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,19 +1,25 @@
-import { DUMMY_DATA } from "../../utils/constants";
+import { useFetch } from "./../../util-hooks/useFetch";
 import classes from "./MeetupItem.module.css";
 import Card from "../ui/Card";
 
 export default function MeetupItem() {
-  const data = DUMMY_DATA[0];
+  const { data } = useFetch({
+    url: "/data.json",
+  });
+
+  if (!data) return <p>Loading...</p>;
+  let [item] = data;
+
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={data.image} alt={data.title} />
+          <img src={item.image} alt={item.title} />
         </div>
         <div className={classes.content}>
-          <h3>{data.title}</h3>
-          <address>{data.address}</address>
-          <p>{data.description}</p>
+          <h3>{item.title}</h3>
+          <address>{item.address}</address>
+          <p>{item.description}</p>
         </div>
         <div className={classes.actions}>
           <button>To favorites</button>
