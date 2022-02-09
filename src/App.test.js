c6fd3b030ec9c2c4a645d469dea66d1d861cb561
David Gras <davidgrcr@gmail.com>
2022-02-09 23:57:37 +0100

    Adding testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable testing-library/no-debugging-utils */
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  //  console.log(wrapper.debug());
+  expect(wrapper.exists()).toBe(true);
 });
